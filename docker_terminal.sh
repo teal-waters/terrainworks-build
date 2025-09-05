ba64a148700a42a5a314b182344e1aaa22306e74
Jesus M Rueda-Becerril <jm.ruebe@gmail.com>
2025-09-05 11:52:19 -0700

    Refactor docker_terminal.sh to accept app name and version as command-line arguments
    - Backing up original Makefile

--- docker_terminal.sh
+++ docker_terminal.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# TODO: make these variables command arguments
-APP_VERSION="0.1"
-APP_NAME="bldgrds"
+APP_NAME="$1"
+APP_VERSION="$2"
+
+if [ -z "$APP_NAME" ] || [ -z "$APP_VERSION" ]; then
+    echo "Usage: $0 <app_name> <app_version>"
+    exit 1
+fi
 
 docker exec -it ${APP_NAME}-${APP_VERSION} /bin/bash
