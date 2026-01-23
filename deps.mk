02b780ac320279a2d9acabcef0d9e2d56bf4594d
Jesus Rueda-Becerril <jm.ruebe@gmail.com>
2026-01-23 14:46:54 -0800

    feat: add new modules and update dependencies in deps.mk

--- deps.mk
+++ deps.mk
@@ -1,20 +1,24 @@
-modules/data_modules.o : modules/data_modules.f90 
-modules/error_handler.o : modules/error_handler.f90 modules/data_modules.o 
-modules/Utilities.o : modules/Utilities.f90 modules/OrderPack/mrgrnk.o modules/error_handler.o modules/data_modules.o 
-modules/TIFF_module.o : modules/TIFF_module.f90 modules/TIFF_LZW_Module.o modules/error_handler.o modules/Grid_Module.o modules/Utilities.o modules/data_modules.o 
-modules/TIFF_LZW_Module.o : modules/TIFF_LZW_Module.f90 modules/error_handler.o modules/data_modules.o 
-modules/DataTableModule.o : modules/DataTableModule.f90 modules/OrderPack/mrgrnk.o modules/Grid_Module.o modules/Utilities.o modules/error_handler.o modules/Utilities.o modules/data_modules.o 
-modules/Grid_Module.o : modules/Grid_Module.f90 modules/TIFF_LZW_Module.o modules/OrderPack/refsor.o modules/Utilities.o modules/Utilities.o modules/error_handler.o modules/data_modules.o 
-modules/ValleyFloor_Module.o : modules/ValleyFloor_Module.f90 modules/Utilities.o modules/ChannelNode_Module.o modules/DEM_module.o modules/Grid_Module.o modules/error_handler.o modules/data_modules.o 
-modules/filters.o : modules/filters.f90 modules/Grid_Module.o modules/OrderPack/mrgrnk.o modules/error_handler.o modules/Utilities.o modules/data_modules.o 
-modules/DEM_module.o : modules/DEM_module.f90 modules/edgeHeap.o modules/OrderPack/mrgrnk.o modules/filters.o modules/DataTableModule.o modules/Utilities.o modules/Utilities.o modules/Grid_Module.o modules/error_handler.o modules/data_modules.o 
-modules/ChannelNode_Module.o : modules/ChannelNode_Module.f90 modules/maxheap.o modules/OrderPack/refsor.o modules/OrderPack/mrgrnk.o modules/Grid_Module.o modules/DataTableModule.o modules/DEM_module.o modules/Utilities.o modules/Utilities.o modules/error_handler.o modules/Piece_Module.o modules/data_modules.o 
-modules/ChannelNetworks.o : modules/ChannelNetworks.f90 modules/Utilities.o modules/Grid_Module.o modules/DEM_module.o modules/ChannelNode_Module.o modules/Utilities.o modules/DataTableModule.o modules/error_handler.o modules/data_modules.o 
-modules/edgeHeap.o : modules/edgeHeap.f90 modules/data_modules.o 
-modules/Piece_Module.o : modules/Piece_Module.f90 modules/random.o modules/error_handler.o modules/Utilities.o modules/data_modules.o 
-modules/random.o : modules/random.f90 modules/data_modules.o 
-modules/maxheap.o : modules/maxheap.f90 modules/data_modules.o 
-modules/OrderPack/refsor.o : modules/OrderPack/refsor.f90 
-modules/OrderPack/mrgrnk.o : modules/OrderPack/mrgrnk.f90 modules/data_modules.o 
-GridUtilities/MakeGrids.o : GridUtilities/MakeGrids.f90 modules/ChannelNetworks.o modules/ChannelNetworks.o modules/ChannelNode_Module.o modules/DEM_module.o modules/Grid_Module.o modules/Utilities.o modules/Utilities.o modules/error_handler.o modules/data_modules.o 
-GridUtilities/bldGrds2.o : GridUtilities/bldGrds2.f90 modules/ChannelNode_Module.o modules/DataTableModule.o modules/DEM_module.o modules/Grid_Module.o modules/Utilities.o modules/Utilities.o modules/error_handler.o modules/data_modules.o 
+modules/data_modules.o : modules/data_modules.f90
+modules/error_handler.o : modules/error_handler.f90 modules/data_modules.o
+modules/Utilities.o : modules/Utilities.f90 modules/OrderPack/mrgrnk.o modules/error_handler.o modules/data_modules.o
+modules/TIFF_module.o : modules/TIFF_module.f90 modules/TIFF_LZW_Module.o modules/error_handler.o modules/Grid_Module.o modules/Utilities.o modules/data_modules.o
+modules/TIFF_LZW_Module.o : modules/TIFF_LZW_Module.f90 modules/error_handler.o modules/data_modules.o
+modules/DataTableModule.o : modules/DataTableModule.f90 modules/OrderPack/mrgrnk.o modules/Grid_Module.o modules/Utilities.o modules/error_handler.o modules/data_modules.o
+modules/Grid_Module.o : modules/Grid_Module.f90 modules/TIFF_LZW_Module.o modules/OrderPack/refsor.o modules/Utilities.o modules/error_handler.o modules/data_modules.o
+modules/ValleyFloor_Module.o : modules/ValleyFloor_Module.f90 modules/Utilities.o modules/ChannelNode_Module.o modules/DEM_module.o modules/Grid_Module.o modules/error_handler.o modules/data_modules.o
+modules/filters.o : modules/filters.f90 modules/Grid_Module.o modules/OrderPack/mrgrnk.o modules/error_handler.o modules/Utilities.o modules/data_modules.o
+modules/DEM_module.o : modules/DEM_module.f90 modules/edgeHeap.o modules/OrderPack/mrgrnk.o modules/filters.o modules/DataTableModule.o modules/Utilities.o modules/Grid_Module.o modules/error_handler.o modules/data_modules.o
+modules/ChannelNode_Module.o : modules/ChannelNode_Module.f90 modules/maxheap.o modules/OrderPack/refsor.o modules/OrderPack/mrgrnk.o modules/Grid_Module.o modules/DataTableModule.o modules/DEM_module.o modules/Utilities.o modules/error_handler.o modules/Piece_Module.o modules/data_modules.o
+modules/ChannelNetworks.o : modules/ChannelNetworks.f90 modules/Utilities.o modules/Grid_Module.o modules/DEM_module.o modules/ChannelNode_Module.o modules/DataTableModule.o modules/error_handler.o modules/data_modules.o
+modules/edgeHeap.o : modules/edgeHeap.f90 modules/data_modules.o
+modules/Piece_Module.o : modules/Piece_Module.f90 modules/random.o modules/error_handler.o modules/Utilities.o modules/data_modules.o
+modules/random.o : modules/random.f90 modules/data_modules.o
+modules/maxheap.o : modules/maxheap.f90 modules/data_modules.o
+modules/OrderPack/refsor.o : modules/OrderPack/refsor.f90
+modules/OrderPack/mrgrnk.o : modules/OrderPack/mrgrnk.f90 modules/data_modules.o
+modules/utils.o : modules/utils.f90 modules/data_modules.o modules/Utilities.o
+modules/kernel_module.o : modules/kernel_module.f90 modules/data_modules.o modules/Utilities.o modules/DEM_module.o modules/utils.o
+modules/derivs_module.o : modules/derivs_module.f90 modules/data_modules.o modules/error_handler.o modules/Utilities.o
+modules/surface_fit.o : modules/surface_fit.f90 modules/data_modules.o modules/error_handler.o modules/Utilities.o modules/utils.o modules/Grid_Module.o modules/DEM_module.o modules/DataTableModule.o modules/kernel_module.o modules/derivs_module.o
+GridUtilities/build_derivs.o : GridUtilities/build_derivs.f90 modules/data_modules.o modules/error_handler.o modules/Utilities.o modules/Grid_Module.o modules/DEM_module.o modules/DataTableModule.o modules/derivs_module.o modules/utils.o modules/surface_fit.o modules/kernel_module.o
+GridUtilities/bldGrds2.o : GridUtilities/bldGrds2.f90 modules/ChannelNode_Module.o modules/DataTableModule.o modules/DEM_module.o modules/Grid_Module.o modules/Utilities.o modules/error_handler.o modules/data_modules.o
